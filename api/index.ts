79d5e264793f2cf863c5f40b771957c40739ce59
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-28 15:54:18 +0200

    riprovo

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,15 @@
 import app from '../src/index.js';
-export default app; 
+import { Env } from "bun"
+import { Context } from "elysia"
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: Context,
+    
+  ): Promise<Response> {
+
+    return await app.fetch(request)
+  },
+}
